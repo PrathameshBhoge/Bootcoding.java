8b6f21197d7dfc43129a1118c2bd40db63686e6e
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-04 20:25:10 +0530

    To copy the Even Element in an array

--- src/main/java/com/bootcoding/array/CopyEvenElement.java
+++ src/main/java/com/bootcoding/array/CopyEvenElement.java
@@ -1,14 +1,24 @@
 package main.java.com.bootcoding.array;
 
 public class CopyEvenElement {
-    public static void main(String [] args){
-        int [] arr={10,20,33,49,50};
-        int [] arr2= new int[arr.length];
-        for(int i =0;i<arr.length;i++){
-           if(arr[i]%2==0){
-               arr2[i]=arr[i];
-               System.out.print(arr2[i]+" ");
-           }
+    public static void main(String[] args) {
+        int[] arr = {10, 23, 33, 49, 50};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+        int index=0;
+        int[] arr2 = new int[count];
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] % 2 == 0) {
+                arr2[index++] = arr[j];
+            }
+        }
+
+        for(int k = 0;k<count;k++){
+            System.out.print(arr2[k]+" ");
         }
     }
 }
