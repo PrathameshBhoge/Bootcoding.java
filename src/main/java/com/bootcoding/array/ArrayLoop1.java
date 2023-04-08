38194b337c6181191423578d3f20d852dbc4c949
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-08 22:30:04 +0530

    To print array element.

--- src/main/java/com/bootcoding/array/ArrayLoop1.java
+++ src/main/java/com/bootcoding/array/ArrayLoop1.java
@@ -4,7 +4,7 @@ public class ArrayLoop1 {
     public static void main(String [] args){
         int [] ages= {10,20,30,40,50};
 
-        for(int i=0;i<=ages.length;i++){
+        for(int i=0;i<ages.length;i++){
             System.out.println("Array Element : "+ ages[i]);
         }
     }
