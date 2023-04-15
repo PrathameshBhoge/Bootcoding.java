d7bdda064236e268021ef1791961f5067a22a5c3
DMX-Sandy <102478169+DMX-Sandy@users.noreply.github.com>
2023-04-15 22:54:34 +0530

    To find wether the length of the to array are same or not.

---
+++ src/main/java/com/bootcoding/array35Q/SizeOfArrayEqual.java
@@ -0,0 +1,14 @@
+package main.java.com.bootcoding.array35Q;
+
+public class SizeOfArrayEqual {
+    public static void main(String[] args) {
+        int [] arr={10,20,30,40,50};
+        int [] arr1={60,70,80,90,100};
+        if(arr.length==arr1.length){
+            System.out.println("Length of the both array is equal.");
+        }else{
+            System.out.println("Length of the both array is not equal.");
+        }
+    }
+
+}
